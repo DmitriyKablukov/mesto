6957d2dff5e466a3012c851f31ba530d5565cf00
DmitriyKablukov <dima-kablukov@ya.ru>
2023-07-10 20:04:03 +0300

    Итог после ревью 3

--- script/index.js
+++ script/index.js
@@ -1,30 +1,27 @@
-
-const editForm = document.querySelector('.popup');
-const editButtonOpen = document.querySelector('.profile__edit-button');
-const editButtonClose = editForm.querySelector('.popup__close-button');
-let editFormPlace = editForm.querySelector('.popup__form');
-let editFormName = editFormPlace.querySelector('.popup__input_data_name');
-let editFormDescription = editFormPlace.querySelector('.popup__input_data_description');
+const popup = document.querySelector('.popup');
+const profileEditButton = document.querySelector('.profile__edit-button');
+const popupCloseButton = popup.querySelector('.popup__close-button');
+let popupForm = popup.querySelector('.popup__form');
+let userName = popupForm.querySelector('.popup__input_data_name');
+let userDescription = popupForm.querySelector('.popup__input_data_description');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 
 const editFormToggle = function(){
-    editForm.classList.toggle('popup_opened');
-    editFormName.value = profileName.textContent;
-    editFormDescription.value = profileDescription.textContent;
+    userName.value = profileName.textContent;
+    userDescription.value = profileDescription.textContent;
+    popup.classList.toggle('popup_opened');
 }
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
-    let profileNameValue = editFormName.value;
-    let profileDescriptionValue = editFormDescription.value;
+    let profileNameValue = userName.value;
+    let profileDescriptionValue = userDescription.value;
     profileName.textContent = profileNameValue;
     profileDescription.textContent = profileDescriptionValue;
     editFormToggle();
 }
 
-editButtonOpen.addEventListener("click", editFormToggle);
-editButtonClose.addEventListener("click", editFormToggle);
-editFormPlace.addEventListener('submit', handleFormSubmit);
-
-
+profileEditButton.addEventListener("click", editFormToggle);
+popupCloseButton.addEventListener("click", editFormToggle);
+popupForm.addEventListener('submit', handleFormSubmit);
